3fc3e0640e1b158dc433d4645343df18b32fe6d6
thomastepi <tepexminem2013@gmail.com>
2022-12-09 16:08:33 -0500

    solo mode 99% complete

--- scripts/index0.js
+++ scripts/index0.js
@@ -1,36 +1,32 @@
-let board = [["", "", ""], ["", "", ""], ["", "", ""]];
-let player;
-let computer;
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+let player1;
+let player2;
 let squaresLeft = 9;
 let cpuRow;
 let cpuColumn;
 const xbuttonElement = document.getElementById("xbutton");
 const obuttonElement = document.getElementById("obutton");
-var result = document.querySelector('.result');
-
-var chrt = document.getElementById('chart').getContext('2d');
-
-var player_score = 0;
-var computer_score = 0;
-
-var player_score_span = document.getElementById('player1Score');
-var computer_score_span = document.getElementById('computerScore');
+var result = document.querySelector(".result");
 
 // Assigns the player and computer as X or O based on the button pressed at the start of the game, if O is chosen the computer moves first
 const chooseXorO = (symbol) => {
   if (symbol === "X") {
-    player = "X";
-    computer = "O";
+    player1 = "X";
+    player2 = "O";
     disableXO(symbol);
     enableBoard();
   } else {
-    player = "O";
-    computer = "X";
+    player1 = "O";
+    player2 = "X";
     disableXO(symbol);
     enableBoard();
-    computerMove();
+    // computerMove();
   }
-}
+};
 
 // Disables the X and O buttons after they are clicked to start the game
 const disableXO = (symbol) => {
@@ -47,104 +43,49 @@ const disableXO = (symbol) => {
     obuttonElement.disabled = true;
     obuttonElement.style.color = "orange";
   }
-}
+};
 
-// Enables clicking of buttons on the board 
+// Enables clicking of buttons on the board
 const enableBoard = () => {
   var boxes = document.getElementsByClassName("box");
   for (var i = 0; i < boxes.length; i++) {
     boxes[i].innerHTML = "";
     boxes[i].setAttribute("onclick", "userMove(id)");
   }
-}
+};
 
 // When the user clicks a square, disables the square, and tests if the player has won, if it's a tie game, then generates a computer move and checks if the computer has won
 const userMove = (id) => {
   document.getElementById(id).removeAttribute("onclick");
-  document.getElementById(id).innerText = player;
+  document.getElementById(id).innerText = player1;
 
-  board[id[0]][id[1]] = player;
+  board[id[0]][id[1]] = player1;
   squaresLeft--;
-  if (testWin(player)) {
-    player_score++
-    check_number_of_wins(player_score);
-    console.log(player_score, computer_score);
-    player_score_span.innerHTML = player_score;
+  if (testWin(player1)) {
     return;
   }
-  if(!testTie()) {
-    setTimeout(function() {
+  if (!testTie()) {
+    setTimeout(function () {
       computerMove();
-  }, 250);
+    }, 250);
   }
-}
+};
 
 // Generates a random computer move in an empty square and disables that square
 const computerMove = () => {
-  do {
-    generateRandomMove();
-  } while (
-    board[cpuRow][cpuColumn] === player ||
-    board[cpuRow][cpuColumn] === computer
+  while (
+    board[cpuRow][cpuColumn] === player1 ||
+    board[cpuRow][cpuColumn] === player2
   );
-  board[cpuRow][cpuColumn] = computer;
-  document.getElementById(cpuRow + "" + cpuColumn).innerHTML = computer;
+  board[cpuRow][cpuColumn] = player2;
+  document.getElementById(cpuRow + "" + cpuColumn).innerHTML = player2;
   document.getElementById(cpuRow + "" + cpuColumn).removeAttribute("onclick");
   squaresLeft--;
-  if (testWin(computer)) {
-    computer_score++
-    check_number_of_wins(computer_score)
-    console.log(player_score, computer_score)
-    computer_score_span.innerHTML = computer_score;
+  if (testWin(player2)) {
     return;
   }
   testTie();
 }
-
-
-// build chart after first player to win 6 times
-function check_number_of_wins(score){
-  var number_of_plays = 0;
-  while (number_of_plays<=10){
-    if(score == 3){
-     create_chart(player_score, computer_score)
-     computer_score = 0;
-     player_score = 0;
-    }
-    number_of_plays++
-    }
-    
-  }
-
-// create chart using scores from game after 10 tries
-function create_chart(p_score, comp_score){
-  var x = [player, computer];
-  var y = [p_score, comp_score];
-  var z = ['red', 'green']
-  
-  var mychart = new Chart(chrt, {
-    type: 'bar',
-    data: {
-      labels: x,
-      datasets: [{
-        label: 'Player and computer scores compared',
-        data: y,
-        backgroundColor: z
-      }]
-    },
-    options: {
-      responsive: true,
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
-  }
-  })
-}
-
 
 // Tests if 3 symbols in a row are the same and ends the game if true
 const testWin = (symbol) => {
@@ -172,21 +113,21 @@ const testWin = (symbol) => {
       board[2][2] === symbol) ||
     (board[0][2] === symbol && board[1][1] === symbol && board[2][0] === symbol)
   ) {
-      result.classList.add('win');
-      result.textContent = symbol + " WINS!"
+    result.classList.add("win");
+    result.textContent = symbol + " WINS!";
     disableBoard();
-    setTimeout(function() {
-    gameReset();
-  }, 4000);
+    setTimeout(function () {
+      gameReset();
+    }, 4000);
     return true;
   }
-}
+};
 
-// Random number generation for use in computer square selection
-const generateRandomMove = () => {
-  cpuRow = Math.floor(Math.random() * 3);
-  cpuColumn = Math.floor(Math.random() * 3);
-}
+// // Random number generation for use in computer square selection
+// const generateRandomMove = () => {
+//   cpuRow = Math.floor(Math.random() * 3);
+//   cpuColumn = Math.floor(Math.random() * 3);
+// }
 
 // Disables clicking of buttons on the board
 const disableBoard = () => {
@@ -194,24 +135,28 @@ const disableBoard = () => {
   for (var i = 0; i < boxes.length; i++) {
     boxes[i].removeAttribute("onclick");
   }
-}
+};
 
 // Tests if all the squares are full, and if so ends the game in a tie
 const testTie = () => {
   if (squaresLeft === 0) {
-      result.classList.add('win');
-      result.textContent = "It's a tie game! Try Again";
+    result.classList.add("win");
+    result.textContent = "It's a tie game! Try Again";
     disableBoard();
-    setTimeout(function() {
+    setTimeout(function () {
       gameReset();
     }, 4000);
     return true;
   }
-}
+};
 
 // Resets the Board
 const gameReset = () => {
-  board = [["", "", ""], ["", "", ""], ["", "", ""]];
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
   squaresLeft = 9;
   xbuttonElement.disabled = false;
   xbuttonElement.style.color = "white";
@@ -219,12 +164,12 @@ const gameReset = () => {
   obuttonElement.disabled = false;
   obuttonElement.style.color = "white";
   obuttonElement.style.border = "1px solid white";
-  result.classList.remove('win');
-  result.textContent = '';
+  result.classList.remove("win");
+  result.textContent = "";
   var boxes = document.getElementsByClassName("box");
   for (var i = 0; i < boxes.length; i++) {
     boxes[i].innerHTML = "";
     boxes[i].setAttribute("onclick", "userMove(id)");
     disableBoard();
   }
-}
+};
